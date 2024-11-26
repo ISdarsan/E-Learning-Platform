e01828736d00920fa1f1d42e56e6330981e5f31f
ISdarsan <isdarsan@gmail.com>
2024-11-26 23:59:25 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const courseCardsContainer = document.querySelector('.course-cards');
+
+    fetch('http://54.226.152.102:3000/api/courses')
+        .then(response => response.json())
+        .then(courses => {
+            courses.forEach(course => {
+                const courseCard = document.createElement('div');
+                courseCard.classList.add('course-card');
+                courseCard.innerHTML = `
+                    <h3>${course.course_name}</h3>
+                    <p>${course.description}</p>
+                    <p>Instructor: ${course.instructor}</p>
+                    <p>Duration: ${course.duration} days</p>
+                    <p>Start Date: ${new Date(course.start_date).toLocaleDateString()}</p>
+                    <p>End Date: ${new Date(course.end_date).toLocaleDateString()}</p>
+                `;
+                courseCardsContainer.appendChild(courseCard);
+            });
+        })
+        .catch(error => console.error('Error fetching courses:', error));
+
+    // Add event listeners for navigation links
+    document.getElementById('login-link').addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = 'login.html';
+    });
+
+    document.getElementById('admin-link').addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = 'admin.html';
+    });
+
+    document.getElementById('about-link').addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = 'aboutus.html';
+    });
+
+    document.getElementById('contactus').addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = 'contactus.html';
+    });
+});
